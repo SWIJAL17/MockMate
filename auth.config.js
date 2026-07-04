70b7ed80e1505310fc37bcad6ac77118b1e3fb81
SWIJAL17 <swijalgedam17@gmail.com>
2026-07-04 22:22:40 +0530

    resolved

--- auth.config.js
+++ auth.config.js
@@ -2,6 +2,7 @@ import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
 
 export const authConfig = {
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
